fb770ecdfec22412cb9e60245f8d2939e45c2d3a
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-27 17:57:06 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+class Shoe# Ma
+  attr_accessor :brand, :color, :size, :material
+  attr_reader :properties, :condition
+
+  def initialize(brand)
+    @shoe = shoe
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+  end
+
+end
+
+shoe = Shoe.new
+shoe.condition
