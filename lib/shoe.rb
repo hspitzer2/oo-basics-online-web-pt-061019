58b592987c65e6c69dc2fc277d8814f6f452d49b
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-27 18:01:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,16 +1,5 @@
-class Shoe# Ma
-  # attr_accessor :brand, :color, :size, :material
-  # attr_reader :properties, :condition
+class Shoe
 
-  def initialize(brand)
-    @shoe = shoe
-  end
 
-  def cobble
-    puts "Your shoe is as good as new!"
-  end
 
 end
-
-shoe = Shoe.new
-shoe.condition
