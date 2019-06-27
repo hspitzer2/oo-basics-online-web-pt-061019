bf71fedef119d82b26d370502f650238263cd7bb
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-27 18:47:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
-  attr_accessor :brand, :color, :size, :material
-  attr_writer   :condition
+  attr_accessor :color, :size, :material, :condition
+  attr_writer   :brand
 
   def initialize(brand)
     @brand = brand
