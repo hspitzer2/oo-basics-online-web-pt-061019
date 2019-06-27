78f582f54328a840f68553d163699d5408c4f689
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-27 18:40:53 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,6 +5,15 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
+  
+  def condition(condition) 
+    @condition = condition
+  end
+  
+  def condition
+    @condition
+  end
+  
   def cobble
     puts "Your shoe is as good as new!"
   end
